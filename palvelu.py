1e29244bc24fc5cec9bd6ee377010e666c941b9b
Metroacorn <ossi.kyhala@gmail.com>
2026-04-24 13:15:00 +0300

    ekat versiot

---
+++ palvelu.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/tulos")
+def nayta_tulos():
+    luku1 = request.args.get("luku1")
+    luku2 = request.args.get("luku2")
+    if luku1 and luku2 < 10:
+        try: return "Nöiden lukujen tulo on %s" % (float(luku1) * float(luku2))
+    else:
+        try: return "Näiden lukujen summa on %s" % (float(luku1) + float(luku2))
+    except: return "EI"
+
+
+@app.route("/seikkailu")
+def nayta_laatikko_sivu():
+    return """
+        <p>laita tähän kaksi jännittävää lukua:
+        <form action="/tulos" enctype="application/x-www-urlencoded"> 
+        Eka luku: <input name=luku1 type=text> <br>
+        Toka luku: <input name=luku2 type=text> <br>
+        <input type=submit value="Tee jotain">
+        </form>
+"""
+
+@app.route("/boom")
+def boom():
+    return "boom"
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ wsgi.py
@@ -0,0 +1,16 @@
+from wsgiref.simple_server import make_server
+
+def app(environ, respond):
+    respond('200 OK', [('Content-type', 'text/html; charset=utf-8')])
+    yield "<p>" .encode("utf-8")
+    yield """<form enctype="application/x-www-urlencoded">""".encode("utf-8")
+    yield """<input name=kysely type=text value="">""".encode("utf-8")
+    yield "</form>".encode("utf-8")
+    yield "<p>".encode("utf-8")
+    yield "environ[QUERY_STRING]".encode("utf-8")
+
+
+
+if __name__ == '__main__':
+    with make_server("localhost", 8000, app) as server: 
+        server.serve_forever()
